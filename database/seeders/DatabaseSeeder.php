c82f11059dea01548740d3b1e8b130416a0e15c9
mauricio <mauri.loboss@gmail.com>
2022-12-17 17:02:06 -0300

    commit funcional

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Perro;
+use App\Models\Interaccion;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+
+        $perro = Perro::factory()->create();
+
+        $posts = Interaccion::factory()
+            ->count(10)
+            ->create();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\perroController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('/tinder')->group(function () use ($router) {
+    $router->post('/postPerro', [PerroController::class, 'guardarPerro']);
+    $router->get('/getPerro', [PerroController::class, 'verPerro']);
+    $router->post('/borrarPerro', [PerroController::class, 'eliminarPerro']);
+    $router->post('/actualizarPerro', [PerroController::class, 'actualizarPerro']);
+
+    $router->get('/verInteresados', [PerroController::class, 'verInteresados']);
+
+
+    $router->post('/postInteraccion', [PerroController::class, 'guardarInteraccion']);
 });
